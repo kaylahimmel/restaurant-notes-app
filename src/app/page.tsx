5c8928802c50a16e2fffc1cafe105748765385e5
Kayla Himmelberger <kaylahimmelberger@gmail.com>
2025-11-16 15:20:11 -0800

    added basic MVP nav for now

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,26 @@
 import styles from './page.module.scss';
+import { Link } from '@/components/Link/Link';
 
 export default function Home() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
         <h1 className={styles.title}>Restaurant Notes</h1>
+        <p>Keep track of your favorite restaurants and dining experiences</p>
+
+        <div className={styles.ctas}>
+          <Link label="Get Started" href="/dashboard" className="primary" />
+          <Link
+            label="Search Restaurants"
+            href="/search"
+            className="secondary"
+          />
+        </div>
+
+        <div className={styles.ctas}>
+          <Link label="Login" href="/auth" className="secondary" />
+        </div>
+
         <link
           rel="stylesheet"
           href="https://fonts.googleapis.com/icon?family=Material+Icons"
